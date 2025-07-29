76747ae218ba77ca2859d9afeccebca01801b2e6
Muhammad-Munsif <m.munsif4375@gmail.com>
2025-07-29 22:57:23 +0500

    small changes in header

--- sidebar-animation.js
+++ sidebar-animation.js
@@ -59,7 +59,7 @@ document.addEventListener("DOMContentLoaded", () => {
     document.addEventListener("click", (event) => {
       dropdownToggles.forEach((toggle) => {
         const dropdownMenu = toggle.nextElementSibling;
-        if (!toggle.contains(event.target) && !dropdownMenu.contains(event.target)) {
+        if (toggle.contains(event.target) && dropdownMenu.contains(event.target)) {
           dropdownMenu.classList.remove("show-dropdown");
         }
       });
